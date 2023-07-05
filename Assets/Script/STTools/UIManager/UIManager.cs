a9b896927f592dbd98399c503223e1e43f0ee30c
Jerrysheen <Jerryshin961115@gmail.com>
2023-07-05 21:46:03 +0800

    D: easy touch component

---
+++ Assets/Script/STTools/UIManager/UIManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UIManagement
+{
+    public interface IHomeUI
+    {
+        /// <summary>
+        /// OnOpen 为打开这个UI的时候做的操作
+        /// </summary>
+        /// <param name="datas"></param>
+        void OnOpen(params object[] datas);
+
+        /// <summary>
+        /// OnClose 为关闭这个UI时候做的操作
+        /// </summary>
+        /// <param name="datas"></param>
+        void OnClose();
+
+        void OnUpdate(params object[] datas);
+    }
+
+    public class UIManager : Singleton<UIManager>
+    {
+        public Dictionary<string, IHomeUI> _uiList = new Dictionary<string, IHomeUI>();
+
+        public void AddUI(IHomeUI ui)
+        {
+
+            var uiname = ui.GetType().ToString();
+
+            Debug.Log(uiname);
+            if (!_uiList.ContainsKey(uiname))
+            {
+                _uiList.Add(uiname, ui);
+            }
+        }
+
+        public void RemoveUI(IHomeUI ui)
+        {
+            var uiname = ui.GetType().ToString();
+            
+            if (_uiList.ContainsKey(uiname))
+            {
+                _uiList.Remove(uiname);
+            }
+        }
+
+        public void Open<T>(params object[] datas) where T : IHomeUI
+        {
+            var uiname = typeof(T).ToString();
+            
+            if (_uiList.TryGetValue(uiname, out IHomeUI ui))
+            {
+                ui.OnOpen(datas);
+            }
+        }
+        
+        public void Close<T>() where T : IHomeUI
+        {
+            var uiname = typeof(T).ToString();
+            
+            if (_uiList.TryGetValue(uiname, out IHomeUI ui))
+            {
+                ui.OnClose();
+            }
+        }
+
+        public void Clear()
+        {
+            if (_uiList != null)
+            {
+                _uiList.Clear();
+            }
+        }
+    }
+}
---
+++ Assets/Script/STTools/UIManager/UIProgressPanel.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UIManagement;
+using System.Linq;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine.UIElements;
+
+namespace UIManagement
+{
+    public enum DialogueDisplayState
+    {
+        Show, Disable
+    }
+
+    public class UIProgressPanel : MonoBehaviour, IHomeUI
+    {
+        
+        public GameObject rootCanvasGO;
+
+        public UnityEngine.UI.Slider slider;
+
+
+        public void UpdateSliderBar(float value)
+        {
+            slider.value = value;
+        }
+
+
+        private void Start()
+        {
+
+            if (UIManager.Instance != null)
+            {
+                UIManager.Instance.AddUI(this);
+            }
+
+            rootCanvasGO = this.gameObject;
+            slider = this.GetComponent<UnityEngine.UI.Slider>();
+            Show();
+        }
+
+        private void OnDestroy()
+        {
+            if(UIManager.Instance != null)
+            UIManager.Instance.RemoveUI(this);
+        }
+
+
+        public void OnOpen(params object[] datas)
+        {   
+            // Show();
+            // 打开的时候， 根据ScriptableObject来读取数据,
+            // todo: 数据类型检测？
+            Show(datas);
+        }
+
+        public void OnClose()
+        {
+            Hide();
+        }
+
+        public void OnUpdate(params object[] datas)
+        {
+            float value = (float)datas[0];
+            UpdateSliderBar(value);
+        }
+
+        private void Show(params object[] datas)
+        {
+            if(!rootCanvasGO.activeSelf)rootCanvasGO.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            StartCoroutine(DelayDisable(4.5f));
+            //rootCanvasGO.SetActive(false);
+        }
+
+        IEnumerator DelayDisable(float time)
+        {
+            yield return new WaitForSeconds(time / 2.0f);
+            this.gameObject.SetActive(false);
+        }
+
+
+        public AudioClip phoneMusic;
+        public AudioClip buttonClickMusic;
+        public void PlayPhoneMusic()
+        {
+            var Go = GameObject.Find("OtherSound");
+            if (!Go) return;
+            AudioSource tempSource = Go.GetComponent<AudioSource>();
+            tempSource.clip = phoneMusic;
+            tempSource.loop = false;
+            tempSource.Play();
+        }
+
+        public void PlayButtonClickMusic()
+        {
+            var Go = GameObject.Find("OtherSound");
+            if (!Go) return;
+            AudioSource tempSource = Go.GetComponent<AudioSource>();
+            tempSource.clip = buttonClickMusic;
+            tempSource.loop = false;
+            tempSource.Play();
+            
+        }
+    }
+}
